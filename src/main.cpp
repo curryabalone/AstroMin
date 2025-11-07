1b6c4bd2958f9d656da6295de4ad2a6b96362c9e
curryabalone <145168341+curryabalone@users.noreply.github.com>
2025-11-06 20:19:52 -0500

    partial real time data logging complete

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,9 @@
 // ==== SD Pins ====
 #define SD_CS 4
 
+// ==== HIGH G IMU Pins ======
+#define HIGH_G_IMU_PIN 27
+
 // ==== Logging Parameters ====
 #define LOG_FREQ_HZ 1000
 #define QUEUE_LENGTH 2000
@@ -13,26 +16,31 @@
 // ==== Data Struct ====
 struct LogData {
   uint32_t timestamp;
-  float value;
+  float x;
+  float y;
+  float z;
 };
 
 // ==== FreeRTOS Handles ====
 QueueHandle_t logQueue;
 TaskHandle_t sdTaskHandle;
+TaskHandle_t HIGH_G_IMU_TaskHandle;
 hw_timer_t* hwTimer = NULL;
 
 // ==== SD ====
 File dataFile;
 
 // ==== ISR: runs at 1 kHz on Core 0 ====
-void IRAM_ATTR onTimer() {
-  static LogData sample;
-  sample.value = micros()-sample.timestamp;
-  sample.timestamp = micros();
-
+void IRAM_ATTR HIGH_G_IMU_ISR() {
   BaseType_t xHigherPriorityTaskWoken = pdFALSE;
-  xQueueSendFromISR(logQueue, &sample, &xHigherPriorityTaskWoken);
+  vTaskNotifyGiveFromISR(HIGH_G_IMU_TaskHandle, &xHigherPriorityTaskWoken);
   if (xHigherPriorityTaskWoken) portYIELD_FROM_ISR();
+}
+// ==== HIGH G IMU Task: runs on Core 0 ====
+void Task_Log_High_G_IMU(void* parameter){
+  for(;;){
+    ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+  }
 }
 
 // ==== SD Task: runs on Core 1 ====
@@ -67,9 +75,11 @@ void TaskSD(void* parameter) {
   }
 }
 
+
+
 void setup() {
   Serial.begin(115200);
-  pinMode(A0, INPUT);
+  pinMode(HIGH_G_IMU_PIN, INPUT);
 
   // Create queue for samples
   logQueue = xQueueCreate(QUEUE_LENGTH, sizeof(LogData));
@@ -80,12 +90,14 @@ void setup() {
 
   // Create SD writer task on core 1
   xTaskCreatePinnedToCore(TaskSD, "SDWriter", 16384, NULL, 1, &sdTaskHandle, 1);
+  xTaskCreatePinnedToCore(Task_Log_High_G_IMU, "", 5000, NULL, 1, &HIGH_G_IMU_TaskHandle, 0); //change stack size if needed later
 
   // ==== Configure hardware timer on core 0 ====
   hwTimer = timerBegin(0, 80, true); // 80 prescaler → 1 µs ticks at 80 MHz APB
-  timerAttachInterrupt(hwTimer, &onTimer, true);
-  timerAlarmWrite(hwTimer, 500, true); // 1000 µs = 1 kHz
+  timerAlarmWrite(hwTimer, 500, true); // 1000 µs = 1 kHz for running the scheduler
   timerAlarmEnable(hwTimer);
+
+  attachInterrupt(HIGH_G_IMU_PIN, &HIGH_G_IMU_ISR, true);
 
   Serial.println("Logging started at 1 kHz");
 }
